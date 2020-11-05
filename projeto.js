87a1e6599cbddde64763feeb05bbc9cf0936e70b
Gabrielly <gabyslvjesus@gmail.com>
2020-11-04 23:32:06 -0300

    Segundo commit

--- projeto.js
+++ projeto.js
@@ -7,7 +7,29 @@ document.querySelector("#botao").addEventListener("dblclick", () => {
 
 document.querySelector("#botao").addEventListener("click", () =>{
     let nome = document.getElementById("nome").value;
-    let sobrenome = document.getElementById("sobrenome").valeu;
+    let sobrenome = document.getElementById("sobrenome").value;
 
     alert("O nome digitado é "+nome +" "+sobrenome);
 })
+
+document.querySelector("#btn").addEventListener("click",() => {
+    let n1,n2,total;
+    n1= parseInt(document.getElementById("n1").value);
+    n2= parseInt(document.getElementById("n2").value);
+    
+    total = n1+n2;
+
+    total2 = n1-n2;
+
+    total3 = n1*n2;
+
+if (n1>0 && n2>0) {
+    total4 = n1/n2;
+}
+else { (total4 = "Não foi possível calcular") }
+    
+
+    alert("O Resultado dos números escolhidos são, Soma = "+total+" , Subtração = "+ total2+" , Multiplicação = "+total3+", Divisão ="+total4);
+})
+
+
